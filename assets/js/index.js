6325aa0964200bb5eb294b8f7e00e53948f57f5f
programminginfotech2023 <programminginfotech2022@gmail.com>
2023-01-16 22:47:26 +0530

    trial16/jan

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,12 @@
+
+//   ------------------ Loader ----------------------------------
+
+$(window).load(function () {
+    $(".spinner").delay(1000).fadeOut("fast");
+});
+
+//   ------------------ Navbar ----------------------------------
+
 (function () {
     "use strict";
 
@@ -41,21 +50,21 @@
 
 })()
 
-var animateButton = function(e) {
+var animateButton = function (e) {
 
     e.preventDefault;
     //reset animation
     e.target.classList.remove('animate');
-    
+
     e.target.classList.add('animate');
-    setTimeout(function(){
-      e.target.classList.remove('animate');
-    },700);
-  };
-  
-  var bubblyButtons = document.getElementsByClassName("bubbly-button");
-  
-  for (var i = 0; i < bubblyButtons.length; i++) {
+    setTimeout(function () {
+        e.target.classList.remove('animate');
+    }, 700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
     bubblyButtons[i].addEventListener('click', animateButton, false);
-  }
+}
 
